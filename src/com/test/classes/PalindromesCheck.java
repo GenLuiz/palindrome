bba508fd95f5fdbbedb505bdf5b63a3eb2a199ee
Barbosa, Luiz <luiz.barbosa@wipro.com>
2017-09-20 16:44:46 -0300

    first commit

---
+++ src/com/test/classes/PalindromesCheck.java
@@ -0,0 +1,57 @@
+package com.test.classes;
+
+public class PalindromesCheck {
+	
+	public Boolean checkPalindromes(String word)
+	{
+		Boolean response = false;
+		
+		word = word.trim();
+		Integer len = word.length();
+		
+		int lastPos = len-1;
+		
+		for(int x = 0; x<len; x++)
+		{
+			if(len%2 == 0)
+			{
+				if (word.charAt((len/2)) == word.charAt((len/2)-1))
+				{
+					if(verifyFirstAndLast(word, x, lastPos)){
+						lastPos--;
+						response = true;
+					}else
+					{
+						return false;
+					}
+				}else
+				{
+					return false;
+				}
+			}else
+			{
+				if(verifyFirstAndLast(word, x, lastPos))
+				{
+					lastPos--;
+					response = true;
+				}else
+				{
+					return false;
+				}
+			}
+		}
+		return response;
+	}
+
+	public Boolean verifyFirstAndLast(String word, int pos1, int pos2)
+	{
+		Boolean responseFrom = false;
+		
+		if(word.charAt(pos1) == word.charAt(pos2))
+		{
+			responseFrom = true;
+		}
+		
+		return responseFrom;
+	}
+}
---
+++ src/com/test/main/Main.java
@@ -0,0 +1,28 @@
+package com.test.main;
+
+import java.util.Scanner;
+
+import com.test.classes.PalindromesCheck;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		PalindromesCheck checker = new PalindromesCheck();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Insira uma palavra:");
+		String word = sc.nextLine();
+		
+		Boolean response = checker.checkPalindromes(word);
+		
+		if(response)
+			System.out.println("A palavra inserida É palíndromo");
+		else
+			System.out.println("A palavra inserida NÃO É palíndromo");
+		
+	}
+
+}
